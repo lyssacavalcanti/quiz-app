aed9d87cef62e565b9c258120d8e53c8f229769d
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2020-09-22 23:33:11 -0300

    Coding update.

---
+++ script.js
@@ -0,0 +1,9 @@
+const quizData = [
+    {
+        question: 'Quantos habitantes existem no mundo em 2020?'
+        a_1: '5 Bi'
+        a_2: '7 Bi'
+        a_3: '8 Bi'
+        a_4: '10 Bi'
+    }
+]
