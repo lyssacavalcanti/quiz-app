9838d5fe624f34da209c87678cf565f3ae506248
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2020-09-23 11:05:55 -0300

    Coding update.

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const quizData = [
     {
-        question: 'Quantos habitantes existem no mundo em 2020?'
-        a_1: '5 Bi'
-        a_2: '7 Bi'
-        a_3: '8 Bi'
-        a_4: '10 Bi'
+        question: 'Quantos habitantes existem no mundo em 2020?',
+        a: '5 Bi',
+        b: '7 Bi',
+        c: '8 Bi',
+        d: '10 Bi',
+        correct: 'c'
+    }, {
+        question: 'Quem disse a famosa frase “Penso, logo existo”?',
+        a: 'Galileu Galilei',
+        b: 'Sócrates',
+        c: 'Descartes',
+        d: 'Platão',
+        correct: 'c'
+    }, {
+        question: 'Quais são os respectivos planetas do sistema solar?',
+        a: 'Mercúrio, Vênus, Terra, Marte, Júpiter, Saturno, Urano e Netuno',
+        b: 'Netuno, Vênus, Terra, Marte, Mercúrio, Saturno, Urano e Júpiter',
+        c: 'Terra, Netuno, Marte, Vênus, Saturno, Júpiter, Mercúrio, e Urano',
+        d: 'Urano, Mercúrio, Netuno, Júpiter, Marte, Vênus, Saturno e Terra',
+        correct: 'a'
+    }, {
+        question: 'Quais as cores da reciclagem do papel, do vidro, do metal e do plástico, respectivamente?',
+        a: 'Vermelho, amarelo, verde e azul',
+        b: 'Verde, vermelho, azul e amarelo',
+        c: 'Amarelo, azul, vermelho e verde',
+        d: 'Azul, verde, amarelo e vermelho',
+        correct: 'd'
     }
 ]
